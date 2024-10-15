46903a48b2c44543c592c3beb51ca92cd604cbc0
Veshsh <isip_i.i.evskin@mpt.ru>
2024-10-15 08:21:14 +0000

    auth_add

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace OpenAPIArtonit.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        [HttpGet, Route("/login")]
+        public IActionResult Login(string username, string pasword)//метод авторизации 
+        {
+            if (username == "ADMIN" || pasword == "333")
+            {
+                var token = CreateJwtToken();
+                return Ok(new { Status = true, Message = "success", Data = new {Token =token} });
+            }
+            return BadRequest();
+        }
+        private string CreateJwtToken() {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+            };
+            var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is my custom Secret key for authentication"));
+            var token = new JwtSecurityToken(
+                expires: DateTime.Now.AddDays(1),
+                claims: claims,
+                signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddWindowsService();//служба
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddAuthentication(c => {
+    c.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    c.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    c.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(c => 
+{
+    c.SaveToken = true;
+    c.RequireHttpsMetadata = true;
+    c.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters 
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        IssuerSigningKey =new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is my custom Secret key for authentication"))
+    };
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => 
+{
+    c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme 
+    { 
+        In = ParameterLocation.Header,
+        Description ="Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat ="JWT",
+        Scheme = "bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference =new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment() || true) //убрать потом
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
